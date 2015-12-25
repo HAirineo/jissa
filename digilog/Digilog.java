0501d46cdb3ab650755cbe267bdcb437b0c68404
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-25 20:31:05 +0800

    Java version of the project

---
+++ digilog/Digilog.java
@@ -0,0 +1,15 @@
+public class Digilog{
+	public static void main(String[] args) {
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		System.out.print("Enter number to convert: ");
+		float decimalPart = decimal(in.next(), in.nextInt());
+	}
+	static float decimal(String input, int base){
+		float val = 0;
+		return val;
+	}
+	static int whole(String input, int base){
+		int val = 0;
+		return val;
+	}
+}
