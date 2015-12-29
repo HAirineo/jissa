bdb9e1d464cd61b796015276bc5ab3461f3c218d
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-29 20:00:31 +0800

    Renamed digilog/given.py to digilog/main.py

---
+++ digilog/main.py
@@ -0,0 +1,36 @@
+import digilog, math, re
+
+def convert_result(result, target):
+	return;
+
+def decimal_separate(expr):
+	"Returns the separated whole number and decimal number"
+	whole_number = ""; decimal_number = ""
+	for x in range(len(expr)):
+		if expr[x] == ".":
+			break
+		else:
+			whole_number += expr[x]
+			decimal_number = expr.replace(expr[x], "")
+			decimal_number = decimal_number.replace(".", "")
+	return int(whole_number), int(decimal_number);
+
+def evaluate_expression(expr, base):
+	"Returns the evaluated expression"
+	answer = ""
+	regexpr = r'\d+|[+/*-]'
+	regexpr_eval = re.findall(regexpr, expr)
+	for x in range(len(regexpr_eval)):
+		if not regexpr_eval[x] == "+":
+			answer += str(digilog.whole_evaluate(str(decimal_separate(regexpr_eval[x])[0]), base) + digilog.decimal_evaluate(str(decimal_separate(regexpr_eval[x])[1]), base))
+		else:
+			answer += regexpr_eval[x]
+	return eval(answer);
+
+def main():
+	expr = raw_input("Enter an expression: ")
+	base = input("Enter the base of the numbers: ")
+	target = input("Enter the target base for the solution: ")
+	print convert_result(evaluate_expression(expr, base), target)
+
+main()
