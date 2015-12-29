ce31b4f50652009961ed2edbe3face43d81658bb
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-29 19:00:41 +0800

    Evaluation of converted math expr done!

--- digilog/given.py
+++ digilog/given.py
@@ -1,16 +1,24 @@
 import digilog, math, re
-# Use the math.eval() method to evaluate the expression to be given by the user
 
 def main():
-	regexpr = r'\d+|[+/*-]'
 	expr = raw_input("Enter an expression: ")
-	elem = re.findall(regexpr, expr)
-	print elem
-	# base = input("Enter the base of the number: ")
-	# print digilog.whole_evaluate(str(decimal_separate(number)[0]), base) + digilog.decimal_evaluate(str(decimal_separate(number)[1]), base)	
+	base = input("Enter the base of the numbers: ")
+	print evaluate_expression(expr, base)
 
-# decimal_separate(expr) separates the whole number and decimal number of an expression
+def evaluate_expression(expr, base):
+	"Returns the evaluated expression"
+	answer = ""
+	regexpr = r'\d+|[+/*-]'
+	regexpr_eval = re.findall(regexpr, expr)
+	for x in range(len(regexpr_eval)):
+		if not regexpr_eval[x] == "+":
+			answer += str(digilog.whole_evaluate(str(decimal_separate(regexpr_eval[x])[0]), base) + digilog.decimal_evaluate(str(decimal_separate(regexpr_eval[x])[1]), base))
+		else:
+			answer += regexpr_eval[x]
+	return eval(answer);
+
 def decimal_separate(expr):
+	"Returns the separated whole number and decimal number"
 	whole_number = ""; decimal_number = ""
 	for x in range(len(expr)):
 		if expr[x] == ".":
