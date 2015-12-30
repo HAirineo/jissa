3d3f4cbac6caf69c92e1b14d19de94611273c399
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-30 12:21:33 +0800

    decimal_separate() method has been revised

--- digilog/main.py
+++ digilog/main.py
@@ -2,21 +2,16 @@ import digilog, math, re
 
 def main():
 	expr = raw_input("Enter an expression: ")
-	base = input("Enter the base of the numbers: ")
-	# target = input("Enter the target base for the solution: ")
-	print evaluate_expression(expr, base)
+	# base = input("Enter the base of the numbers: ")
+	# print evaluate_expression(expr, base)
+	
+	regexpr = r'\d+[\.]\d+|\d+|[-+/*]'
+	regexpr_eval = re.findall(regexpr, expr)
+	print decimal_separate(regexpr_eval[0])
 
 def decimal_separate(expr):
 	"Returns the separated whole number and decimal number"
-	whole_number = ""; decimal_number = ""
-	for x in range(len(expr)):
-		if expr[x] == ".":
-			break
-		else:
-			whole_number += expr[x]
-			decimal_number = expr.replace(expr[x], "")
-			decimal_number = decimal_number.replace(".", "")
-	return int(whole_number), int(decimal_number);
+	return math.modf(float(expr))[0], math.modf(float(expr))[1]
 
 def evaluate_expression(expr, base):
 	"Returns the evaluated expression"
@@ -24,7 +19,7 @@ def evaluate_expression(expr, base):
 	regexpr = r'\d+|[-+/*]'
 	regexpr_eval = re.findall(regexpr, expr)
 	for x in range(len(regexpr_eval)):
-		if regexpr_eval[x] == "+" or regexpr_eval[x] == "-" or regexpr_eval[x] == "\*" or regexpr_eval[x] == "\/":
+		if regexpr_eval[x] == "+" or regexpr_eval[x] == "-":
 			answer += regexpr_eval[x]
 		else:
 			answer += str(digilog.whole_evaluate(str(decimal_separate(regexpr_eval[x])[0]), base) + digilog.decimal_evaluate(str(decimal_separate(regexpr_eval[x])[1]), base))
