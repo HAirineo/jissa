def97d1624e5bf099004fbe20663bb65230fe320
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-30 20:13:35 +0800

    Added documentation, removal of some string to properly convert the expression to its base-10 equivalent

--- digilog/digilog/Decimal.py
+++ digilog/digilog/Decimal.py
@@ -1,8 +1,9 @@
 def decimal_evaluate(expr, base):
 	"Returns the base-10 equivalent of decimal digits"
+	expr = expr.replace("0.", "") # Remove the string "0."
 	decimal = 0; x = 0; y = -1
 	while x < len(expr) and (abs(y) - 1) < len(expr):
-		decimal += float(expr[x]) * (float(base) ** y)
+		decimal += float(expr[x]) * (float(base) ** y) # Positional notation
 		x += 1
 		y -= 1
 	return decimal
--- digilog/digilog/Whole.py
+++ digilog/digilog/Whole.py
@@ -1,8 +1,9 @@
 def whole_evaluate(expr, base):
 	"Returns the base-10 equivalent of the whole number"
+	expr = expr.replace(".0", "") # Remove the string ".0"
 	whole = 0; x = len(expr) - 1; y = 0
 	while x >= 0 and y < len(expr):
-		whole += int(expr[x]) * (base ** y)
+		whole += int(expr[x]) * (base ** y) # Positional notation
 		x -= 1
 		y += 1
 	return whole
