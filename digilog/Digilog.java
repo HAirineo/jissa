37cc09d26e5c90402f9d41a060a78ce9c5a0f361
AFAgarap <abienfred.agarap@yahoo.com>
2015-12-30 21:50:55 +0800

    Completed the separate(String expr) method

--- digilog/Digilog.java
+++ digilog/Digilog.java
@@ -1,9 +1,22 @@
 public class Digilog{
 	public static void main(String[] args) {
-		
+		System.out.println((int)(separate("123.456").get(0)) + 123);
 	}
 
-	static int separate(String expr){
+	static double decimal(String expr, int base){
+		double dec = 0;
+		for (int x = 0, y = -1; x < expr.length() && (Math.abs(y) - 1) < expr.length(); x++, y--) {
+			dec += (int)(Character.digit(expr.charAt(x), 10)) * Math.pow(base, y);
+		}
+		return dec;
+	}
+
+	static double evaluateExpression(String expr, int base){
+		return 0;
+	}
+
+	static java.util.ArrayList separate(String expr){
+		java.util.ArrayList <Integer> number = new java.util.ArrayList <Integer> ();
 		String val = ""; String dec = "";
 		for (int x = 0; x < expr.length(); x++) {
 			if (expr.charAt(x) == '.') break;
@@ -13,15 +26,9 @@ public class Digilog{
 				dec = dec.substring(dec.indexOf(".") + 1, dec.length());
 			}
 		}
-		return Integer.parseInt(val);
-	}
-
-	static double decimal(String expr, int base){
-		double dec = 0;
-		for (int x = 0, y = -1; x < expr.length() && (Math.abs(y) - 1) < expr.length(); x++, y--) {
-			dec += (int)(Character.digit(expr.charAt(x), 10)) * Math.pow(base, y);
-		}
-		return dec;
+		number.add(Integer.parseInt(val));
+		number.add(Integer.parseInt(dec));
+		return number;
 	}
 
 	static int whole(String expr, int base){
